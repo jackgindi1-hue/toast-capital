705e56687d17bd2e302a78438084c9d5d97647ef
jackgindi1-hue <jackgindi1@gmail.com>
2026-03-12 22:54:52 -0400

    Add UI components and ProgressTracker

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -5,11 +5,8 @@ import * as DialogPrimitive from "@radix-ui/react-dialog"
 import { X } from "lucide-react"
 
 const Dialog = DialogPrimitive.Root
-
 const DialogTrigger = DialogPrimitive.Trigger
-
 const DialogPortal = DialogPrimitive.Portal
-
 const DialogClose = DialogPrimitive.Close
 
 const DialogOverlay = React.forwardRef<
